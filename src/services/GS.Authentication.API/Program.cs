713487ca2ab0cd1f872c2fa49e83d31087c649f0
Wesley <wesleyferreirasb@gmail.com>
2022-03-04 00:55:15 -0300

    add default identity

--- src/services/GS.Authentication.API/Program.cs
+++ src/services/GS.Authentication.API/Program.cs
@@ -1,5 +1,6 @@
 using GS.Authentication.API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(
         options.UseSqlServer(
             builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+builder.Services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
