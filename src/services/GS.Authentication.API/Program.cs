e6f3fae878d38ea0c55dcf3deee3525661fb7c8e
Wesley <wesleyferreirasb@gmail.com>
2022-03-04 09:21:54 -0300

    configuring project to support documentation with swagger

--- src/services/GS.Authentication.API/Program.cs
+++ src/services/GS.Authentication.API/Program.cs
@@ -1,11 +1,12 @@
 using GS.Authentication.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(
-    options => 
+    options =>
         options.UseSqlServer(
             builder.Configuration.GetConnectionString("DefaultConnection"))
 );
@@ -14,9 +15,36 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc(name: "v1", new OpenApiInfo
+    {
+        Title = "Game Store Authentication API",
+        Description = "This is a API for study of technologies used in enterprise applications",
+        Contact = new OpenApiContact() { Name = "Wesley Ferreira", Email = "wesleyferreirasb@gmail.com" },
+        License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/license/MIT") }
+    });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(config =>
+{
+    config.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
