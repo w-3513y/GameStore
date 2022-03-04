722abb1f481c64457f05d4f45afb248013587eb7
Wesley <wesleyferreirasb@gmail.com>
2022-03-04 09:32:20 -0300

    configuring APi documentation

--- src/services/GS.Authentication.API/Controllers/AuthenticationController.cs
+++ src/services/GS.Authentication.API/Controllers/AuthenticationController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GS.Authentication.API.Controllers;
 
+[ApiController]
 [Route("api/authentication")]
 public class AuthenticationController : Controller
 {
