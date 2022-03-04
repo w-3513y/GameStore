2cb1392bb5a098aacfbcf19733d8e9927b2f4ffb
Wesley <wesleyferreirasb@gmail.com>
2022-03-04 09:23:43 -0300

    adjust error: class fields didn't appear

--- src/services/GS.Authentication.API/Models/UserCreate.cs
+++ src/services/GS.Authentication.API/Models/UserCreate.cs
@@ -6,12 +6,12 @@ public class UserCreate
 {
     [Required(ErrorMessage = "the field {0} is required)")]
     [EmailAddress(ErrorMessage = "{0} invalid")]
-    public string Email;
+    public string Email {get; set;} = "";
 
     [Required(ErrorMessage = "the field {0} is required")]
     [EmailAddress(ErrorMessage = "{0} invalid")]
-    public string Password;
+    public string Password {get; set;} = "";
 
     [Compare("Password", ErrorMessage = "the password is diferent")]
-    public string PasswordConfirmation {get; set;}
+    public string PasswordConfirmation {get; set;} = "";
 }
--- src/services/GS.Authentication.API/Models/UserLogin.cs
+++ src/services/GS.Authentication.API/Models/UserLogin.cs
@@ -6,10 +6,10 @@ public class UserLogin
 {
     [Required(ErrorMessage = "the field {0} is required)")]
     [EmailAddress(ErrorMessage = "{0} invalid")]
-    public string Email;
+    public string Email {get; set;} = "";
 
     [Required(ErrorMessage = "the field {0} is required")]
     [StringLength(100, ErrorMessage = "the length of the field need to be between {2} and {1}", MinimumLength = 6)]
-    public string Password;
+    public string Password {get; set;} = "";
 
 }
