525956d303c8c4b66285b67deec7ec389aa208e0
Wesley <wesleyferreirasb@gmail.com>
2022-03-05 20:44:59 -0300

    excludes ununsed launchsettings

--- src/services/GS.Authentication.API/Models/UserResponse.cs
+++ src/services/GS.Authentication.API/Models/UserResponse.cs
@@ -6,7 +6,6 @@ public class UserResponse
 {
     public string AccessToken {get; set;}
     public double ExpiresIn {get; set;}
-
     public UserToken userToken {get; set;}
 
 }
--- src/services/GS.Authentication.API/Models/UserToken.cs
+++ src/services/GS.Authentication.API/Models/UserToken.cs
@@ -6,7 +6,6 @@ public class UserToken
 {
     public string Id {get; set;}
     public string Email {get; set;}
-
     public IEnumerable<UserClaim> Claims {get; set;}
 
 }
