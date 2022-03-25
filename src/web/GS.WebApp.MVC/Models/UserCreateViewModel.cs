02951ea9718ea2e7ac14b2b99b293bd77eac8b84
Wesley <wesleyferreirasb@gmail.com>
2022-03-25 19:23:43 -0300

    created razor pages and view models for authentication

---
+++ src/web/GS.WebApp.MVC/Models/UserCreateViewModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GS.WebApp.MVC.Models;
+
+public class UserCreate
+{
+    [Required(ErrorMessage = "the field {0} is required)")]
+    [EmailAddress(ErrorMessage = "{0} invalid")]
+    public string Email {get; set;} = "";
+
+    [Required(ErrorMessage = "the field {0} is required")]
+    [EmailAddress(ErrorMessage = "{0} invalid")]
+    public string Password {get; set;} = "";
+
+    [Compare("Password", ErrorMessage = "the password is diferent")]
+    public string PasswordConfirmation {get; set;} = "";
+}
---
+++ src/web/GS.WebApp.MVC/Models/UserLoginViewModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GS.WebApp.MVC.Models;
+
+public class UserLogin
+{
+    [Required(ErrorMessage = "the field {0} is required)")]
+    [EmailAddress(ErrorMessage = "{0} invalid")]
+    public string Email {get; set;} = "";
+
+    [Required(ErrorMessage = "the field {0} is required")]
+    [StringLength(100, ErrorMessage = "the length of the field need to be between {2} and {1}", MinimumLength = 6)]
+    public string Password {get; set;} = "";
+
+}
