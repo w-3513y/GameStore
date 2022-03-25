a9a3778853fd6ced05fcfcd1a5d72e05aa84e4ef
Wesley <wesleyferreirasb@gmail.com>
2022-03-25 18:12:29 -0300

    included cookies support

--- src/web/GS.WebApp.MVC/Configuration/IdentityConfig.cs
+++ src/web/GS.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -4,11 +4,17 @@ namespace GS.WebApp.MVC.Configuration;
 
 public static class IdentityConfig
 {
-    public static void AddIdentityConfiguration(this IServiceCollection services) 
+    public static void AddIdentityConfiguration(this IServiceCollection services)
     {
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.LoginPath = "/login";
+            options.AccessDeniedPath = "/access-denied";
+        });
     }
 
-    public static void UseIdentityConfiguration(this IApplicationBuilder app) 
+    public static void UseIdentityConfiguration(this IApplicationBuilder app)
     {
         app.UseAuthentication();
         app.UseAuthorization();
