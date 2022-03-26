b6ea1b736f1b63742318c9c09e4ec9a16b9aa18c
Wesley <wesleyferreirasb@gmail.com>
2022-03-25 22:53:31 -0300

    temporarly solution to error: AuthenticationException: The remote certificate is invalid because of errors in the certificate chain: PartialChain

--- src/web/GS.WebApp.MVC/Services/AuthenticationService.cs
+++ src/web/GS.WebApp.MVC/Services/AuthenticationService.cs
@@ -14,21 +14,35 @@ public class AuthenticationService : IAuthenticationService
 
     public async Task<string> Signin(UserLoginViewModel user)
     {
+        HttpClientHandler clientHandler = new HttpClientHandler();
+        clientHandler.ServerCertificateCustomValidationCallback =
+                (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+        // Pass the handler to httpclient(from you are calling api)
+        HttpClient client = new HttpClient(clientHandler);
+
         var content = new StringContent(content: JsonSerializer.Serialize(user),
                                         encoding: Encoding.UTF8,
                                         mediaType: "application/json");
-        var response = await _httpClient.PostAsync(requestUri: Url.signIn,
-                                                   content: content);
+        var response = await client.PostAsync(requestUri: Url.signIn,
+            content: content);
         return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
     }
 
     public async Task<string> Signup(UserCreateViewModel user)
     {
+        HttpClientHandler clientHandler = new HttpClientHandler();
+        clientHandler.ServerCertificateCustomValidationCallback =
+                (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+        // Pass the handler to httpclient(from you are calling api)
+        HttpClient client = new HttpClient(clientHandler);
+
         var content = new StringContent(content: JsonSerializer.Serialize(user),
                                         encoding: Encoding.UTF8,
                                         mediaType: "application/json");
-        var response = await _httpClient.PostAsync(requestUri: Url.signUp,
-                                             content: content);
+        var response = await client.PostAsync(requestUri: Url.signUp,
+            content: content);
         return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
     }
 }
